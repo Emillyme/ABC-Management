135ef887ff822f81cb9a39e6172e1cb501d6ef1f
emillyme <emill.ymello212@gmail.com>
2024-10-28 21:21:38 -0300

    Navbar design sendo terminada

--- client/src/app/components/Navbar/index.tsx
+++ client/src/app/components/Navbar/index.tsx
@@ -1,24 +1,38 @@
-// tsrafce
-import React from 'react'
-import { Settings , Search }from 'lucide-react'
+import React from 'react';
+import { Settings, Search } from 'lucide-react';
+import Link from 'next/link';
+import Image from 'next/image';
+import Logo from '../../assets/abc.svg';
 
-const index = () => {
+const Index = () => {
   return (
-    <div className='flex items-center justify-between bg-white px-4 py-3 dark:bg-black'>
-        {/* Search Bar */}  
-        <div className="flex items-center gap-8">
-            <div className="relative flex h-min w-[200px]">
-                <Search className="absolute left-[4px] top-1/2 -translate-y-1/2 mr-2 h-5 w-5 transform cursor-pointer dark:text-white"/>
-                <input className="w-full rounded border-none bg-gray-100 p-2 pl-8 placeholder-gray-500 focus:border-transparent focus:outline-none dark:bg-gray-700 dark:text-white dark:placeholder-white" type="search" placeholder="Pesquise aqui..."/>
+    <div className="flex items-center justify-between bg-white px-4 py-3 dark:bg-black">
+      {/* Logo */}
+      <div className="flex items-center">
+        <Image src={Logo} alt="Logo" width={150} height={150} className="mr-[30px]" />
+      </div>
 
-                {/* Icons */}
-                <div className="flex items-center">
-                  <Settings/>
-                </div>
-            </div>
+      {/* Search Bar */}
+      <div className="flex items-center gap-8">
+        <div className="relative flex h-min w-[200px]">
+          <Search className="absolute left-[4px] top-1/2 -translate-y-1/2 mr-2 h-5 w-5 transform cursor-pointer dark:text-white" />
+          <input
+            className="w-full rounded border-none bg-gray-100 p-2 pl-8 placeholder-gray-500 focus:border-transparent focus:outline-none dark:bg-gray-700 dark:text-white dark:placeholder-white"
+            type="search"
+            placeholder="Pesquise aqui..."
+          />
         </div>
-    </div>
-  )
-}
+      </div>
 
-export default index;
+      {/* Icons para o usuario*/}
+      <div className="flex items-center ml-auto">
+        <Link href="/settings" className="h-min w-min rounded p-2 hover:bg-gray-100">
+          <Settings className="h-5 w-6 cursor-pointer dark:text-white" />
+        </Link>
+        <div className="ml-2 min-h-[2em] w-[0.1rem] bg-gray-200 hidden md:inline-block"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Index;
